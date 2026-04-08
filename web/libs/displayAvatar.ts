d27dbe640db1dfee4c2bed92be282ad9cb54747a
dangkhoa35 <khoa030577@gmail.com>
2026-04-08 11:30:44 +0700

    Delete web directory

--- web/libs/displayAvatar.ts
+++
@@ -1,7 +0,0 @@
-const DEFAULT_BLUE_AVATAR = "/images/default-blue.png";
-export const DEFAULT_AVATAR_SRC = DEFAULT_BLUE_AVATAR;
-
-export const getHeaderAvatarSrc = (image?: string | null) => {
-  if (image && image.trim() !== "") return image;
-  return DEFAULT_BLUE_AVATAR;
-};
